772a343b93c76ae84546351f18831942177e9403
santiago clemenzi <clemenzisantiago@gmail.com>
2022-06-17 20:30:24 -0300

    posicionamineto de iconos

--- src/components/home/modules/Icons.jsx
+++ src/components/home/modules/Icons.jsx
@@ -3,10 +3,32 @@ import * as React from 'react';
 // MUI
 import { Grid } from '@mui/material';
 
+// Icons
+import { ReactComponent as IconLinkedIn } from '../../../icons/linkedin-3.svg';
+import { ReactComponent as InconGithub } from '../../../icons/github-3.svg';
+
 const Icons = () => {
 	return (
-		<Grid item>
-			<p>Iconos de LinkedIn y git hub</p>
+		<Grid
+			container
+			lg={12}
+			md={12}
+			sm={12}
+			xs={12}
+			sx={{ marginBottom: '1%' }}
+			direction="row"
+			justifyContent="flex-start"
+			alignItems="center"
+		>
+			<Grid item sx={{ marginBottom: '0.5%' }}>
+				<h3>&lt;/santi&gt;</h3>
+			</Grid>
+			<Grid item sx={{ marginRight: '0.5%', marginLeft: '1%' }}>
+				<IconLinkedIn />
+			</Grid>
+			<Grid item sx={{ marginLeft: '0.5%' }}>
+				<InconGithub />
+			</Grid>
 		</Grid>
 	);
 };
