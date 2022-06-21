44ff739cc2b974baf67cd34c74e21fd64c2166ed
santiago clemenzi <clemenzisantiago@gmail.com>
2022-06-21 19:07:53 -0300

    creamos seccion AboutMe

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
 // components
 import Home from './components/home/Home';
+import AboutMe from './components/aboutMe/AboutMe';
 
 // MUI
-// import { createTheme, ThemeProvider } from '@mui/system';
 import { Box } from '@mui/material';
 
 
 function App() {
 	return (
-		<Box sx={{ bgcolor: '#121212 ', color: '#fff' }}>
+		<Box sx={{ bgcolor: '#121212 ', color: '#fff', marginBottom: '0px' }}>
 			<Home />
+			<AboutMe />
 		</Box>
 	);
 }
---
+++ src/components/aboutMe/AboutMe.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+const AboutMe = () => {
+    return ( 
+        <div>
+            <h1>About me</h1>
+            <h1>About me</h1>
+            <h1>About me</h1>
+            <h1>About me</h1>
+            <h1>About me</h1>
+        </div>
+      );
+}
+ 
+export default AboutMe;
